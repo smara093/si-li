e797f3a10ffb5ed85129d4e7623bc828abed5bd1
Smaranda Fournier <smaranda.onutu@gmail.com>
2018-01-26 14:04:57 -0500

    Add/remove works properly, list is ordered by date last modified, alphabetically

--- App.js
+++ App.js
@@ -2,56 +2,73 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableHighlight, Button, ListView, TextInput } from 'react-native';
 
 let rows = []
+const now = Date.now();
 
-for(let i=0;i<=20;i++)
-  rows.push({key: i, text: `Item #${i}`})
+for(let i=0;i<=20;i++){
+    rows.push({key: now+i, text: `Item #${i}`, lastModified: now, isActive: true})}
 
 const itemKeyExtractor = ({key}) => key
+
+const groupArrayBy = (arr, key) =>{
+  let reduced = arr.reduce((r, current)=>{
+    (r[current[key]] = r[current[key]] || []).push(current)
+    return r
+  }, {})
+
+  let r = []
+  const keys = Object.keys(reduced).sort().reverse();
+  for(let i=0;i<keys.length;i++)
+    r = r.concat(reduced[keys[i]])
+   return r
+}
+
+const sortByDateDesc = (a, b) =>{
+  if(a.lastModified == b.lastModified) {
+    return a.text.localeCompare(b.text);
+  }
+
+  return b.lastModified-a.lastModified;
+}
 
 export default class App extends React.Component {
   constructor(props){
     super(props)
-    this.state = {activeItems: rows, inactiveItems: [], newItem:''}
+    this.state = {items: groupArrayBy(rows.sort(sortByDateDesc), "isActive"), newItem:''}
   }
 
   removeItem = (index) => {
-    this.state.inactiveItems.push(this.state.activeItems.splice(index, 1))
-    this.setState({activeItems: this.state.activeItems, inactiveItems: this.state.inactiveItems, newItem:this.state.newItem})
+    this.state.items[index].isActive = false
+    this.state.items[index].lastModified = Date.now()
+    this.state.items.sort(sortByDateDesc);
+    this.setState({items: groupArrayBy(this.state.items, "isActive"), newItem:this.state.newItem})
     this.forceUpdate()
   }
 
   renderItem = ({item, index})=>{
-    console.log(`rendering item ${item.text} at index ${index}`)
     return (
       <TouchableHighlight onLongPress={()=>{this.removeItem(index)}}>
         <View style={styles.row}>
-          <Text>{item.text}</Text>
+          <Text>{item.text}-{item.lastModified}-{item.isActive.toString()}</Text>
         </View>
       </TouchableHighlight>
     )
   }
 
   addItem = ({text})=> {
-    console.log(`adding item ${text}`)
-    this.state.activeItems.push({key: this.state.activeItems.length, text: this.newItem})  
-    this.newItem = ''
-    this.setState({activeItems: this.state.activeItems, inactiveItems: this.state.inactiveItems})
+    this.state.items.push({key: Date.now(), text: this.state.newItem, lastModified: Date.now(), isActive: true})  
+    this.setState({items: groupArrayBy(this.state.items.sort(sortByDateDesc), "isActive"), newItem: ''})
     this.forceUpdate()
   }
 
   render() {
+    let newItemValue = ''
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Si[mple]-Li[st]</Text>  
-        <TextInput placeholder='type to add a new item' onChangeText={(text) => this.newItem = text} underlineColorAndroid='transparent' style={styles.newItem}/>
+        <TextInput value={this.state.newItem} placeholder='type to add a new item' onChangeText={(text) => this.setState({items: this.state.items, newItem: text})} underlineColorAndroid='transparent' style={styles.newItem}/>
         <Button title="Add" onPress={this.addItem}/>
         <FlatList
-            data={this.state.activeItems}
-            renderItem={this.renderItem}
-            keyExtractor={itemKeyExtractor}
-            extraData={this.state}/>
-        <FlatList
-            data={this.state.inactiveItems}
+            data={this.state.items}
             renderItem={this.renderItem}
             keyExtractor={itemKeyExtractor}
             extraData={this.state}/>
@@ -67,7 +84,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    backgroundColor: 'purple',
+    backgroundColor: 'pink',
     padding: 15,
     marginBottom: 5,
     alignItems: 'center'
--- README.md
+++ README.md
@@ -7,13 +7,12 @@ This is a simple list app built with React Native and Expo. It's meant to do the
 - add a new item
 - remove an item by long press 
 - display section of removed items at the bottom 
+- order items by date added
 - add some type of persistency, besides state
 
 Stretch goals:
-- order items by date added
 - add an undo option for reverting the previous action
 - add the ability to reactivate an item from the inactive section
 - add the ability to clear inactive items 
 - prompt to cleanup if inactive items gets out of hand (configurable)
 - add configuration menu 
-
