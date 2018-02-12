b8a4178b414b7e7babc98e17755e379b696d7299
Smaranda Fournier <smaranda.onutu@gmail.com>
2018-02-12 08:46:58 -0500

    Updated tests and snapshots

--- app/tests/components.SimpleList.test.js
+++ app/tests/components.SimpleList.test.js
@@ -18,7 +18,7 @@ beforeEach(() => {
     {
       text: 'item 1',
       isActive: true,
-      lastModified: new Date(2017, 1, 1),
+      lastModified: new Date(2017, 1, 1, 1, 0, 0),
       id: 1,
     },
   ];
