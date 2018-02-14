f08b0d4cc7c87a375e3503b505b83b95098a949c
Smaranda Fournier <smaranda.onutu@gmail.com>
2018-02-14 15:42:07 -0500

    Fixed another eslint issue

--- app/screens/login.js
+++ app/screens/login.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { Button } from 'react-native';
 import { PropTypes } from 'prop-types';
 
-export class Login extends React.PureComponent {
+class Login extends React.PureComponent {
   static navigationOptions = {
     title: 'sign in',
   };
 
   render() {
     return (
-      <Button
-        onPress={() => this.props.navigation.navigate('ActiveList')}
-        title="No thanks"
-      />
+      <Button onPress={() => this.props.navigation.navigate('ActiveList')} title="No thanks" />
     );
   }
 }
