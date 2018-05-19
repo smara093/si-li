5f7a08d5707c3283aba21d33f80ec03403a53b91
smaraf <smaranda.onutu@gmail.com>
2018-05-19 17:44:51 -0400

    updated my account title to match other titles

--- app/screens/my-account.js
+++ app/screens/my-account.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
-import { connect } from 'react-redux';
-import { PropTypes } from 'prop-types';
+import React from "react";
+import { Button, Text, View } from "react-native";
+import { connect } from "react-redux";
+import { PropTypes } from "prop-types";
 
-import * as myAccountActions from '../actions/my-account-actions';
+import * as myAccountActions from "../actions/my-account-actions";
 
 class MyAccount extends React.PureComponent {
   static navigationOptions = {
-    title: 'my-account',
+    title: "my account"
   };
 
   render() {
@@ -15,7 +15,8 @@ class MyAccount extends React.PureComponent {
     return (
       <View>
         <Text>
-          You are currently logged in as {currentUser.name} using {currentUser.email}
+          You are currently logged in as {currentUser.name} using{" "}
+          {currentUser.email}
         </Text>
         <Button onPress={onSignOut} title="Sign Out" />
       </View>
@@ -25,17 +26,17 @@ class MyAccount extends React.PureComponent {
 
 MyAccount.propTypes = {
   currentUser: PropTypes.object.isRequired,
-  onSignOut: PropTypes.func.isRequired,
+  onSignOut: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-  currentUser: state.login.currentUser,
+  currentUser: state.login.currentUser
 });
 
 const mapDispatchToProps = dispatch => ({
   onSignOut: () => {
     dispatch(myAccountActions.signOut());
-  },
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(MyAccount);
